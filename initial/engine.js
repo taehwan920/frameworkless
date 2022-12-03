97d6c1b53c4f519fe709eeca21984bc3e732bcd7
Taehwan <taehwan.yang@business-canvas.com>
2022-12-03 15:01:07 +0900

    refactor: 함수 쪼개기

--- initial/engine.js
+++ initial/engine.js
@@ -9,7 +9,6 @@ const state = {
 const main = document.querySelector(".todoapp");
 
 window.requestAnimationFrame(() => {
-  console.log("done?");
   const newMain = view(main, state);
   main.replaceWith(newMain);
 });
