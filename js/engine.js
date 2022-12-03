6532bd4dbd8e08e43ba061e0cbe830ea17398c85
Taehwan <taehwan.yang@business-canvas.com>
2022-12-03 14:33:03 +0900

    feat: 첫 데이터 연결

---
+++ js/engine.js
@@ -0,0 +1,15 @@
+import getTodos from "./getTodos.js";
+import view from "./view.js";
+
+const state = {
+  todos: getTodos(),
+  currentFilter: "All",
+};
+
+const main = document.querySelector(".todoapp");
+
+window.requestAnimationFrame(() => {
+  console.log("done?");
+  const newMain = view(main, state);
+  main.replaceWith(newMain);
+});
